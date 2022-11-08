bddd7343154f2169538c1a2a0d6b1db3ccacb852
Bin Son <ndhs1609@gmail.com>
2022-11-08 14:14:34 +0700

    new

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
-  // images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   constructor() {}
 
   ngOnInit(): void {}
