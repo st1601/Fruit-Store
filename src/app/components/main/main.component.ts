d344b78dde07d00ef283a9194408e561894e7167
Bin Son <ndhs1609@gmail.com>
2022-11-10 08:56:21 +0700

    load

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
-
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {}
+  // private spinner: NgxSpinnerService
+  ngOnInit() {
+    /** spinner starts on init */
+    // this.spinner.show();
+    // setTimeout(() => {
+    //   /** spinner ends after 5 seconds */
+    //   this.spinner.hide();
+    // }, 1000);
   }
-
 }
